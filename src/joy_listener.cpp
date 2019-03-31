0933e6a3a28f1054135006cc5f7dbb27733d4f29
Jens Petit <petit.jens@gmail.com>
2019-03-31 10:46:57 +0000

    added led to indicate received movements

--- src/joy_listener.cpp
+++ src/joy_listener.cpp
@@ -2,6 +2,7 @@
 #include "std_msgs/String.h"
 #include "sensor_msgs/Joy.h"
 #include <rc/motor.h>
+#include <rc/led.h>
 
 void chatterCallback(const sensor_msgs::Joy::ConstPtr& msg)
 {
@@ -10,6 +11,23 @@ void chatterCallback(const sensor_msgs::Joy::ConstPtr& msg)
 
   rc_motor_set(1, msg->axes[1]);
   rc_motor_set(2, msg->axes[3]);
+
+  if (msg->axes[1] > 0.5)
+  {
+    rc_led_set(RC_LED_GREEN, 1);
+  }
+  else 
+  {
+    rc_led_set(RC_LED_GREEN, 0);
+  }
+  if (msg->axes[3] > 0.5)
+  {
+    rc_led_set(RC_LED_RED, 1);
+  }
+  else 
+  {
+    rc_led_set(RC_LED_RED, 0);
+  }
 }
 
 int main(int argc, char **argv)
